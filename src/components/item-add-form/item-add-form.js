2a72656e73a75a440541634dd7c6b6aa2342436a
Sabina <rigby917@gmail.com>
2020-11-13 10:16:31 +0300

    add controlled

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -4,26 +4,42 @@ import './item-add-form.css';
 export default class ItemAddForm extends Component {
 
     state = {
-        label : ''
+        label : '' 
     };
+    // для получения текущего значения интпута и каждый раз когда значение меняется
+    // вызываем эту функцию onLabelChange и используя объект е(ивэнт) мы достаем из него тек. знач. поля
     onLabelChange = (e) => {
+        // обновление стэйта
         this.setState({
-            label: e.target.value
+            // с console.log выводится текущее значение
+            label: e.target.value.toUpperCase()
         });
     }
+       // обработка результатов
+    //    чтобы отловить отправку формы
     onSubmit = (e) => {
+        // когда ивэет будет обрабптывается действие по дефллту отправлять не нунжо.
+        // дефолт для формы - отправка на сервер с перезагрузкой страницы
         e.preventDefault();
-        this.props.onItemAdded(this.state.label)
-    }
+        // передаем значение из поля ввода
+        this.props.onItemAdded(this.state.label);
+        // делаемпустой label строкой  
+        this.setState({
+            label: ''
+        });
+    }; 
 
     render() {
         return (
             <form className="item-add-form d-flex"
+         
                 onSubmit={this.onSubmit}>
                 <input type="text"
                        className="form-control"
                        onChange={this.onLabelChange}
-                placeholder="What needs to be done! "/>
+                placeholder="What needs to be done! "
+                // делаем элемент controlled
+                value = {this.state.label}/>
                 <button className="btn btn-outline-secondary"
                 onClick={() => this.props.onItemAdded('Hello')}>
                     Add item
